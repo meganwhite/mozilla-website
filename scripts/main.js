136f6016890c0f14740bbe39fe6302fab89a4c1f
meganwhite <mfw247@gmail.com>
2018-01-08 15:51:47 -0500

    Added onclick script

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+//Define a function
+function sayOuch() {
+	alert('Ouch! Stop poking me!');
+}
+
+//Use CSS selector to identify an element
+var foxImage = document.querySelector('img');
+
+//Assign the function to the onclick event on the img
+foxImage.onclick = saOuch;
